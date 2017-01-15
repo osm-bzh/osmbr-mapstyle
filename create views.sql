35358a0c495a4f2129a0b8ffa94f28f4bb1e051f
MaelREBOUX <m.reboux@rennesmetropole.fr>
2017-01-15 17:55:08 +0100

    minor correction

--- create views.sql
+++ create views.sql
@@ -1,15 +1,19 @@
 
--- osm_water_areas
-CREATE OR REPLACE VIEW public.osm_water_areas AS
+-- osm_waterareas
+CREATE OR REPLACE VIEW osm_waterareas AS
   SELECT
     osm_id,
-    COALESCE(planet_osm_polygon.tags -> 'name:br'::text) as name,
-    COALESCE(planet_osm_polygon.tags -> 'source:name:br'::text) as source_name,
+    COALESCE(tags -> 'name:br'::text) as name,
+    COALESCE(tags -> 'source:name:br'::text) as source_name,
     COALESCE(waterway, '') || COALESCE(landuse, '') || COALESCE("natural", '') as type,
     way_area as area,
     way as geometry
   FROM planet_osm_polygon
-  WHERE waterway = 'riverbank'::text OR (landuse = ANY (ARRAY['reservoir'::text, 'water'::text, 'basin'::text, 'salt_pond'::text])) OR ("natural" = ANY (ARRAY['lake'::text, 'water'::text])) OR amenity = 'swimming_pool'::text OR leisure = 'swimming_pool'::text
+  WHERE 
+    waterway = 'riverbank'::text
+    OR (landuse = ANY (ARRAY['reservoir'::text, 'water'::text, 'basin'::text, 'salt_pond'::text]))
+    OR ("natural" = ANY (ARRAY['lake'::text, 'water'::text])) OR amenity = 'swimming_pool'::text
+    OR leisure = 'swimming_pool'::text
   ORDER BY z_order, way_area DESC;
 
 
@@ -37,7 +41,5 @@ CREATE OR REPLACE VIEW osm_waterareas_gen0 AS
     geometry
    FROM osm_waterareas_gen1
   WHERE ST_AREA(ST_SimplifyPreserveTopology(geometry, 200)) > 50000.000000
-
-
 
 
