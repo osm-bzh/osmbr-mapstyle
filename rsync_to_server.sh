9932cd14efa336f0193df725dbf677887e9a79df
MaelReboux <m.reboux@rennesmetropole.fr>
2017-02-20 06:04:11 +0100

    maj scripts

--- rsync_to_server.sh
+++ rsync_to_server.sh
@@ -1,1 +1,18 @@
-rsync /data/osm/tiles/br/ osm@breizhpositive.bzh:/data/osm/tiles/org/
+
+opts='-ar --delete'
+
+#rsync $opts /data/osm/tiles/br/8/ osm@breizhpositive.bzh:/data/osm/tiles/br/8/
+
+i=0
+
+while ((i <= 16))
+	do
+	date
+	echo '===> '$i
+	du -sh /data/osm/tiles/br/$i/
+	rsync $opts /data/osm/tiles/br/$i/ osm@breizhpositive.bzh:/data/osm/tiles/br/$i/
+	date
+	((i += 1))
+done
+
+
