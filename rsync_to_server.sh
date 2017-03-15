105ca0c036ff965ea4e925ef22350334dd7e2287
MaelREBOUX <m.reboux@rennesmetropole.fr>
2017-03-15 11:16:47 +0100

    synchro with github
    Conflicts:
    rsync_to_server.sh

--- rsync_to_server.sh
+++ rsync_to_server.sh
@@ -1,17 +1,28 @@
 
-
 opts='-ar --delete'
 
-
+SECONDS=0
+global_sec=0
 i=0
 
 while ((i <= 16))
-        do
-        date
-        echo '===> '$i
-        du -sh /data/osm/tiles/br/$i/
-        rsync $opts /data/osm/tiles/br/$i/ osm@breizhpositive.bzh:/data/osm/tiles/br/$i/
-        date
-        ((i += 1))
+	do
+	SECONDS=0
+	echo '===> '$i
+	du -sh /data/osm/tiles/br/$i/
+	rsync $opts /data/osm/tiles/br/$i/ osm@breizhpositive.bzh:/data/osm/tiles/br/$i/
+	
+	level_duration=$SECONDS
+	global_sec=$((global_sec + level_duration))
+	echo "$(($level_duration / 60)) min and $(($level_duration % 60)) sec"
+	echo ''
+
+	((i += 1))
 done
 
+echo ''
+
+duration=global_sec
+echo "Total time : $(($duration / 60)) min and $(($duration % 60)) sec"
+
+echo ''
---
+++ update_db.sh
@@ -0,0 +1,18 @@
+# on efface les dumps existants
+rm /data/osm/dumps/*.pbf
+
+# on récupère le dernier dump France
+wget -O /data/osm/dumps/ http://download.geofabrik.de/europe/france-latest.osm.pbf
+
+# on le decoupe selon un polygone
+osmconvert france-latest.osm.pbf -B=/data/osm/data/poly_extraction_bzh.poly --complete-ways -v -o=breizh.osm.pbf
+
+# on supprime les vues
+psql -d osm -f drop_views.sql
+
+# on met à jour la base de données
+osm2pgsql -d osm --hstore --slim --cache 2300 -E 3857 breizh.osm.pbf
+
+# on recrée les vues
+psql -d osm -f create_views.sql
+
