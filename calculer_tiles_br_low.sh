bb1c27721532eba16b1fdff6c17223e86f8ca036
MaelREBOUX <m.reboux@rennesmetropole.fr>
2017-03-27 22:57:46 +0200

    update scripts

--- calculer_tiles_br_low.sh
+++ calculer_tiles_br_low.sh
@@ -1,6 +1,3 @@
-
-date
-
 # on efface
 rm -rf /data/osm/tiles/br/0/
 rm -rf /data/osm/tiles/br/1/
@@ -11,12 +8,22 @@ rm -rf /data/osm/tiles/br/5/
 rm -rf /data/osm/tiles/br/6/
 
 # monde -> 4
+SECONDS=0
 render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/osm/tiles/ -z 0 -Z 4
+level_duration=$SECONDS
+echo "$(($level_duration / 60)) min and $(($level_duration % 60)) sec"
+echo ''
 
 # niveau 5 : europe
+SECONDS=0
 render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/osm/tiles/ -z 5 -Z 5 -x 13 -X 9 -y 88 -Y 14
+level_duration=$SECONDS
+echo "$(($level_duration / 60)) min and $(($level_duration % 60)) sec"
+echo ''
 
 # niveau 6 : europe
+SECONDS=0
 render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/osm/tiles/ -z 6 -Z 6 -x 28 -X 37 -y 19 -Y 26
-
-date
+level_duration=$SECONDS
+echo "$(($level_duration / 60)) min and $(($level_duration % 60)) sec"
+echo ''
--- update_db.sh
+++ update_db.sh
@@ -1,3 +1,4 @@
+
 # on efface les dumps existants
 rm /data/osm/dumps/*.pbf
 
@@ -8,11 +9,11 @@ wget -O /data/osm/dumps/ http://download.geofabrik.de/europe/france-latest.osm.p
 osmconvert france-latest.osm.pbf -B=/data/osm/data/poly_extraction_bzh.poly --complete-ways -v -o=breizh.osm.pbf
 
 # on supprime les vues
-psql -d osm -f drop_views.sql
+psql -d osm -f /data/osm/styles/osmbr/drop_views.sql
 
 # on met à jour la base de données
-osm2pgsql -d osm --hstore --slim --cache 2300 -E 3857 breizh.osm.pbf
+osm2pgsql -d osm --hstore --slim --cache 2300 -E 3857 /data/osm/dumps/breizh.osm.pbf
 
 # on recrée les vues
-psql -d osm -f create_views.sql
+psql -d osm -f /data/osm/styles/osmbr/create_views.sql
 
