52e8e222053697e0723180b93904ad8ba1ca9dc2
MaelREBOUX <m.reboux@rennesmetropole.fr>
2017-04-06 21:56:35 +0200

    update db script update

--- update_db.sh
+++ update_db.sh
@@ -3,16 +3,16 @@
 rm /data/osm/dumps/*.pbf
 
 # on récupère le dernier dump France
-wget -O /data/osm/dumps/ http://download.geofabrik.de/europe/france-latest.osm.pbf
+wget -O /data/osm/dumps/france-latest.osm.pbf http://download.geofabrik.de/europe/france-latest.osm.pbf
 
 # on le decoupe selon un polygone
-osmconvert france-latest.osm.pbf -B=/data/osm/data/poly_extraction_bzh.poly --complete-ways -v -o=breizh.osm.pbf
+osmconvert /data/osm/dumps/france-latest.osm.pbf -B=/data/osm/data/poly_extraction_bzh.poly --complete-ways -v -o=breizh.osm.pbf
 
 # on supprime les vues
 psql -d osm -f /data/osm/styles/osmbr/drop_views.sql
 
 # on met à jour la base de données
-osm2pgsql -d osm --hstore --slim --cache 2300 -E 3857 /data/osm/dumps/breizh.osm.pbf
+osm2pgsql -d osm --hstore --slim --cache 2500 -E 3857 /data/osm/dumps/breizh.osm.pbf
 
 # on recrée les vues
 psql -d osm -f /data/osm/styles/osmbr/create_views.sql
