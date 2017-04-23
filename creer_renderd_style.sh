788c4264fdca456e486f77f48d34cf3967a142ea
MaelReboux <m.reboux@rennesmetropole.fr>
2017-04-23 14:35:08 +0200

    no more need replace IP

--- creer_renderd_style.sh
+++ creer_renderd_style.sh
@@ -1,7 +1,9 @@
 #bin/sh!
 
 cd /data/osm/styles/osmbr/
-cp project.mml project_local.mml
-sed -i 's/192.168.0.42/localhost/' project_local.mml
-/data/osm/carto/bin/carto ./project_local.mml > style.xml
+/data/osm/carto/bin/carto ./project.mml > style.xml
+
+#cp project.mml project_local.mml
+#sed -i 's/192.168.0.42/localhost/' project_local.mml
+#/data/osm/carto/bin/carto ./project_local.mml > style.xml
 
