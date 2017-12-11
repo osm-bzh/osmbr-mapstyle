b935b28ad4d5d0bb34300319bc0653f5888f278e
MaelReboux <m.reboux@rennesmetropole.fr>
2017-12-11 21:35:16 +0100

    specfic form osm202

--- calculer_tiles_br_high_osm202.sh
+++ calculer_tiles_br_high_osm202.sh
@@ -13,52 +13,52 @@ rm -rf /data/tiles/br/22/
 
 
 # Pazieg Roazhon Bruz
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 16 -Z 16 -x 32440 -X 32482 -y 22738 -Y 22790
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 17 -Z 17 -x 64882 -X 64965 -y 45477 -Y 45580
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 18 -Z 18 -x 129765 -X 129930 -y 90953 -Y 91160
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 16 -Z 16 -x 32440 -X 32482 -y 22738 -Y 22790
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 17 -Z 17 -x 64882 -X 64965 -y 45477 -Y 45580
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 18 -Z 18 -x 129765 -X 129930 -y 90953 -Y 91160
 
 
 # Gwipavaz
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 16 -Z 16 -x 31958 -y 22569 -X 31975 -Y 22666
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 17 -Z 17 -x 63917 -y 45318 -X 63951 -Y 45332
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 18 -Z 18 -x 127820 -y 90631 -X 127918 -Y 90684
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 16 -Z 16 -x 31958 -y 22569 -X 31975 -Y 22666
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 17 -Z 17 -x 63917 -y 45318 -X 63951 -Y 45332
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 18 -Z 18 -x 127820 -y 90631 -X 127918 -Y 90684
 
 
 # Lokournan - Brest - Daoulas
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 16 -Z 16 -x 31923 -y 22660 -X 31996 -Y 22685
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 17 -Z 17 -x 63846 -y 45320 -X 63989 -Y 45372
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 18 -Z 18 -x 127692 -y 90640 -X 127978 -Y 90743
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 16 -Z 16 -x 31923 -y 22660 -X 31996 -Y 22685
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 17 -Z 17 -x 63846 -y 45320 -X 63989 -Y 45372
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 18 -Z 18 -x 127692 -y 90640 -X 127978 -Y 90743
 
 
 # Lambaol-Blouarzhel
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 16 -Z 16 -x 31897 -y 22653 -X 31904 -Y 22662
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 17 -Z 17 -x 63794 -y 45306 -X 63808 -Y 45323
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 18 -Z 18 -x 127587 -y 90613 -X 127617 -Y 90648  
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 16 -Z 16 -x 31897 -y 22653 -X 31904 -Y 22662
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 17 -Z 17 -x 63794 -y 45306 -X 63808 -Y 45323
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 18 -Z 18 -x 127587 -y 90613 -X 127617 -Y 90648  
 
 
 # Karaez
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 16 -Z 16 -x 32111 -y 22702 -X 32125 -Y 22708
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 17 -Z 17 -x 64223 -y 45404 -X 64251 -Y 45416
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 18 -Z 18 -x 128446 -y 90807 -X 128503 -Y 90833
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 16 -Z 16 -x 32111 -y 22702 -X 32125 -Y 22708
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 17 -Z 17 -x 64223 -y 45404 -X 64251 -Y 45416
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 18 -Z 18 -x 128446 -y 90807 -X 128503 -Y 90833
 
 
 # Lesneven
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 16 -Z 16 -x 31973 -y 22619 -X 31987 -Y 22632
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 17 -Z 17 -x 63946 -y 45237 -X 63976 -Y 45264
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 18 -Z 18 -x 127892 -y 90474 -X 127953 -Y 90528
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 16 -Z 16 -x 31973 -y 22619 -X 31987 -Y 22632
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 17 -Z 17 -x 63946 -y 45237 -X 63976 -Y 45264
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 18 -Z 18 -x 127892 -y 90474 -X 127953 -Y 90528
 
 # Lannuon
 
 # Langoned
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 16 -Z 16 -x 32121 -y 22724 -X 32147 -Y 22764
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 17 -Z 17 -x 64243 -y 45447 -X 64294 -Y 45527
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 18 -Z 18 -x 128486 -y 90893 -X 128588 -Y 91054
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 16 -Z 16 -x 32121 -y 22724 -X 32147 -Y 22764
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 17 -Z 17 -x 64243 -y 45447 -X 64294 -Y 45527
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 18 -Z 18 -x 128486 -y 90893 -X 128588 -Y 91054
 
 
 # Gwened
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 16 -Z 16 -x 32255 -y 22867 -X 32279 -Y 22882
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 17 -Z 17 -x 64509 -y 45734 -X 64558 -Y 45764
-sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 18 -Z 18 -x 129018 -y 91468 -X 129117 -Y 91527
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 16 -Z 16 -x 32255 -y 22867 -X 32279 -Y 22882
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 17 -Z 17 -x 64509 -y 45734 -X 64558 -Y 45764
+sudo -u www-data render_list -a -m br -s /var/run/renderd/renderd.sock -n 8 -t /data/tiles/ -z 18 -Z 18 -x 129018 -y 91468 -X 129117 -Y 91527
 
 
 date
