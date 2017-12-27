07c66b5358737e627e213b6a2e3809130455c3b8
MaelReboux <m.reboux@rennesmetropole.fr>
2017-12-27 19:07:41 +0100

    foreign tables
    first try on VM test

---
+++ create_foreign_tables.sql
@@ -0,0 +1,384 @@
+
+-- this script will create the 7 foreign tables from the world database
+--
+-- planet_osm_point
+-- planet_osm_line
+-- planet_osm_polygon
+-- planet_osm_roads
+-- planet_osm_nodes
+-- planet_osm_ways
+-- planet_osm_rels
+--
+
+
+-- https://wiki.postgresql.org/wiki/Foreign_data_wrappers
+-- http://www.postgresonline.com/journal/archives/322-Generating-Create-Foreign-Table-Statements-for-postgres_fdw.html
+
+-- Adding the postgres_fdw extension
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+-- Adding the hstore extension
+CREATE EXTENSION IF NOT EXISTS hstore;
+
+-- the "server" object
+CREATE SERVER world_db_server
+  FOREIGN DATA WRAPPER postgres_fdw
+  OPTIONS (host 'db.openstreetmap.local', port '5432', dbname 'osm');
+
+-- a mapping for authentification
+CREATE USER MAPPING FOR public SERVER 
+  world_db_server
+  OPTIONS (user 'osm', password 'osmbr');
+
+
+
+
+CREATE FOREIGN TABLE public.planet_osm_point
+(
+    osm_id bigint,
+    access text,
+    "addr:housename" text,
+    "addr:housenumber" text,
+    "addr:interpolation" text,
+    admin_level text,
+    aerialway text,
+    aeroway text,
+    amenity text,
+    area text,
+    barrier text,
+    bicycle text,
+    brand text,
+    bridge text,
+    boundary text,
+    building text,
+    capital text,
+    construction text,
+    covered text,
+    culvert text,
+    cutting text,
+    denomination text,
+    disused text,
+    ele text,
+    embankment text,
+    foot text,
+    "generator:source" text,
+    harbour text,
+    highway text,
+    historic text,
+    horse text,
+    intermittent text,
+    junction text,
+    landuse text,
+    layer text,
+    leisure text,
+    lock text,
+    man_made text,
+    military text,
+    motorcar text,
+    name text,
+    "natural" text,
+    office text,
+    oneway text,
+    operator text,
+    place text,
+    poi text,
+    population text,
+    power text,
+    power_source text,
+    public_transport text,
+    railway text,
+    ref text,
+    religion text,
+    route text,
+    service text,
+    shop text,
+    sport text,
+    surface text,
+    toll text,
+    tourism text,
+    "tower:type" text,
+    tunnel text,
+    water text,
+    waterway text,
+    wetland text,
+    width text,
+    wood text,
+    z_order integer,
+    tags hstore,
+    way geometry(Point,3857)
+)
+SERVER world_db_server
+OPTIONS (schema_name 'public', table_name 'planet_osm_point');
+
+
+
+CREATE FOREIGN TABLE public.planet_osm_line
+(
+    osm_id bigint,
+    access text,
+    "addr:housename" text,
+    "addr:housenumber" text,
+    "addr:interpolation" text,
+    admin_level text,
+    aerialway text,
+    aeroway text,
+    amenity text,
+    area text,
+    barrier text,
+    bicycle text,
+    brand text,
+    bridge text,
+    boundary text,
+    building text,
+    construction text,
+    covered text,
+    culvert text,
+    cutting text,
+    denomination text,
+    disused text,
+    embankment text,
+    foot text,
+    "generator:source" text,
+    harbour text,
+    highway text,
+    historic text,
+    horse text,
+    intermittent text,
+    junction text,
+    landuse text,
+    layer text,
+    leisure text,
+    lock text,
+    man_made text,
+    military text,
+    motorcar text,
+    name text,
+    "natural" text,
+    office text,
+    oneway text,
+    operator text,
+    place text,
+    population text,
+    power text,
+    power_source text,
+    public_transport text,
+    railway text,
+    ref text,
+    religion text,
+    route text,
+    service text,
+    shop text,
+    sport text,
+    surface text,
+    toll text,
+    tourism text,
+    "tower:type" text,
+    tracktype text,
+    tunnel text,
+    water text,
+    waterway text,
+    wetland text,
+    width text,
+    wood text,
+    z_order integer,
+    way_area real,
+    tags hstore,
+    way geometry(LineString,3857)
+)
+SERVER world_db_server
+OPTIONS (schema_name 'public', table_name 'planet_osm_line');
+
+
+
+CREATE FOREIGN TABLE public.planet_osm_polygon
+(
+    osm_id bigint,
+    access text,
+    "addr:housename" text,
+    "addr:housenumber" text,
+    "addr:interpolation" text,
+    admin_level text,
+    aerialway text,
+    aeroway text,
+    amenity text,
+    area text,
+    barrier text,
+    bicycle text,
+    brand text,
+    bridge text,
+    boundary text,
+    building text,
+    construction text,
+    covered text,
+    culvert text,
+    cutting text,
+    denomination text,
+    disused text,
+    embankment text,
+    foot text,
+    "generator:source" text,
+    harbour text,
+    highway text,
+    historic text,
+    horse text,
+    intermittent text,
+    junction text,
+    landuse text,
+    layer text,
+    leisure text,
+    lock text,
+    man_made text,
+    military text,
+    motorcar text,
+    name text,
+    "natural" text,
+    office text,
+    oneway text,
+    operator text,
+    place text,
+    population text,
+    power text,
+    power_source text,
+    public_transport text,
+    railway text,
+    ref text,
+    religion text,
+    route text,
+    service text,
+    shop text,
+    sport text,
+    surface text,
+    toll text,
+    tourism text,
+    "tower:type" text,
+    tracktype text,
+    tunnel text,
+    water text,
+    waterway text,
+    wetland text,
+    width text,
+    wood text,
+    z_order integer,
+    way_area real,
+    tags hstore,
+    way geometry(Geometry,3857)
+)
+SERVER world_db_server
+OPTIONS (schema_name 'public', table_name 'planet_osm_polygon');
+
+
+
+CREATE FOREIGN TABLE public.planet_osm_roads
+(
+    osm_id bigint,
+    access text,
+    "addr:housename" text,
+    "addr:housenumber" text,
+    "addr:interpolation" text,
+    admin_level text,
+    aerialway text,
+    aeroway text,
+    amenity text,
+    area text,
+    barrier text,
+    bicycle text,
+    brand text,
+    bridge text,
+    boundary text,
+    building text,
+    construction text,
+    covered text,
+    culvert text,
+    cutting text,
+    denomination text,
+    disused text,
+    embankment text,
+    foot text,
+    "generator:source" text,
+    harbour text,
+    highway text,
+    historic text,
+    horse text,
+    intermittent text,
+    junction text,
+    landuse text,
+    layer text,
+    leisure text,
+    lock text,
+    man_made text,
+    military text,
+    motorcar text,
+    name text,
+    "natural" text,
+    office text,
+    oneway text,
+    operator text,
+    place text,
+    population text,
+    power text,
+    power_source text,
+    public_transport text,
+    railway text,
+    ref text,
+    religion text,
+    route text,
+    service text,
+    shop text,
+    sport text,
+    surface text,
+    toll text,
+    tourism text,
+    "tower:type" text,
+    tracktype text,
+    tunnel text,
+    water text,
+    waterway text,
+    wetland text,
+    width text,
+    wood text,
+    z_order integer,
+    way_area real,
+    tags hstore,
+    way geometry(LineString,3857)
+)
+SERVER world_db_server
+OPTIONS (schema_name 'public', table_name 'planet_osm_roads');
+
+
+
+
+
+CREATE FOREIGN TABLE public.planet_osm_nodes
+(
+    id bigint NOT NULL,
+    lat integer NOT NULL,
+    lon integer NOT NULL,
+    tags text[]
+)
+SERVER world_db_server
+OPTIONS (schema_name 'public', table_name 'planet_osm_nodes');
+
+
+
+CREATE FOREIGN TABLE public.planet_osm_ways
+(
+    id bigint NOT NULL,
+    nodes bigint[] NOT NULL,
+    tags text[]
+)
+SERVER world_db_server
+OPTIONS (schema_name 'public', table_name 'planet_osm_ways');
+
+
+
+CREATE FOREIGN TABLE public.planet_osm_rels
+(
+    id bigint NOT NULL,
+    way_off smallint,
+    rel_off smallint,
+    parts bigint[],
+    members text[],
+    tags text[]
+)
+SERVER world_db_server
+OPTIONS (schema_name 'public', table_name 'planet_osm_rels');
+
