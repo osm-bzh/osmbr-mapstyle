584af47e8ecf57d4fa6f94200f94c2a57ecc7dc7
MaelReboux <m.reboux@rennesmetropole.fr>
2018-01-03 23:12:29 +0100

    correction pour calcul tuiles low

--- calculer_tiles_br_low.sh
+++ calculer_tiles_br_low.sh
@@ -43,7 +43,7 @@ echo ""
 
 # niveau 5 : europe
 SECONDS=0
-render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 5 -Z 5 -x 13 -X 9 -y 88 -Y 14
+render_list -a -m br -s /var/run/renderd/renderd.sock -n 3 -t /data/tiles/ -z 5 -Z 5 -x 11 -X 20 -y 8 -Y 15
 level_duration=$SECONDS
 echo "$(($level_duration / 60)) min and $(($level_duration % 60)) sec"
 echo ""
