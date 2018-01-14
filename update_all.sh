e2f71819d15f6f4e17166e4991443a30e762c9f7
MaelReboux <m.reboux@rennesmetropole.fr>
2018-01-14 12:11:46 +0100

    arrêt / démarrage de renderd

--- update_all.sh
+++ update_all.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# on arrête le service de rendu
+service renderd stop
+
 # maj de la BDD
 cd /data/dumps/
 sudo -u osm /data/styles/br/update_db.sh
 
+# on redémarre le service de rendu
+service renderd start
 
 # calcul des tuiles
 #sudo -u www-data /data/styles/br/calculer_tiles_br_0-6.sh
