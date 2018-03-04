335fea98ac5edcef91865fa42ff5f21390bd58a5
MaelREBOUX <m.reboux@rennesmetropole.fr>
2018-03-04 16:21:26 +0000

    update script update

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -4,11 +4,10 @@
 /data/styles/br/scripts/update_db.sh
 
 # compute tiles
-#/data/styles/br/scripts/calculer_tiles_br_0-6.sh
-#/data/styles/br/scripts/calculer_tiles_br_7-12.sh
-/data/styles/br/scripts/calculer_tiles_br_13-18.sh
+#sudo -u www-data /data/styles/br/scripts/calculer_tiles_br_0-6.sh
+#sudo -u www-data /data/styles/br/scripts/calculer_tiles_br_7-12.sh
+sudo -u www-data /data/styles/br/scripts/calculer_tiles_br_13-18.sh
 
 # push to www server
-/data/styles/br/scripts/rsync_to_server.sh
-
+#/data/styles/br/scripts/rsync_to_server.sh
 
