fa46610a605ed8438272cbfc0c90acf6ce53d6a0
MaelReboux <m.reboux@rennesmetropole.fr>
2018-03-08 06:12:12 +0100

    no more need foreign tables

--- database/create_db.sh
+++ database/create_db.sh
@@ -13,11 +13,4 @@ psql -c "CREATE DATABASE osm WITH OWNER = osm ENCODING = 'UTF8';"
 psql -d osm -c "CREATE EXTENSION IF NOT EXISTS hstore;"
 psql -d osm -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-# for the foreign tables
-psql -d osm -c "CREATE EXTENSION IF NOT EXISTS postgres_fdw;"
-
-# the "server" object
-psql -d osm -c "CREATE SERVER world_db_server
-  FOREIGN DATA WRAPPER postgres_fdw
-  OPTIONS (host 'db.openstreetmap.local', port '5432', dbname 'osm');"
 
--- database/drop_foreign_tables.sql
+++
@@ -1,9 +0,0 @@
-
-
-DROP FOREIGN TABLE planet_osm_line;
-DROP FOREIGN TABLE planet_osm_nodes;
-DROP FOREIGN TABLE planet_osm_point;
-DROP FOREIGN TABLE planet_osm_polygon;
-DROP FOREIGN TABLE planet_osm_rels;
-DROP FOREIGN TABLE planet_osm_roads;
-DROP FOREIGN TABLE planet_osm_ways;
