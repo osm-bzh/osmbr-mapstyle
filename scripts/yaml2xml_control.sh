e3e99fc31daf6ba85971e1b664e048e3c2083f09
Maël <mael@breizhpodu.local>
2020-04-20 22:27:45 +0200

    switch to yaml
    control layers

---
+++ scripts/yaml2xml_control.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+# permet de passer d'un fichier YAML à un fichier XML exploitable par renderd
+
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  BR"
+
+cp osm_br_control.yml osm_br_control.mml
+carto osm_br_control.mml > osm_br_control.xml
+rm osm_br_control.mml
+
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  OC"
+
+cp osm_oc_control.yml osm_oc_control.mml
+carto osm_oc_control.mml > osm_oc_control.xml
+rm osm_oc_control.mml
+
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  EU"
+
+cp osm_eu_control.yml osm_eu_control.mml
+carto osm_eu_control.mml > osm_eu_control.xml
+rm osm_eu_control.mml
+
+echo ""
+echo ""
+
+ls -laGh osm_*.xml
+git status
+
+#sudo service renderd stop
+#sudo service renderd start
+
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  FIN"
+echo ""
