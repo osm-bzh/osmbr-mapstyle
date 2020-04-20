83133ee7ef4ebd90fa831483b6a4844af0381786
Maël <mael@breizhpodu.local>
2020-04-20 21:28:30 +0200

    switch to yaml
    script python inutilisable -> carto sait gérer yaml -> xml après renommage du fichier

---
+++ scripts/yaml2xml.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+# permet de passer d'un fichier YAML à un fichier XML exploitable par renderd
+
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  BR"
+
+cp osm_br.yml osm_br.mml
+carto osm_br.mml > osm_br.xml
+rm osm_br.mml
+
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  OC"
+
+cp osm_oc.yml osm_oc.mml
+carto osm_oc.mml > osm_oc.xml
+rm osm_oc.mml
+
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  EU"
+
+cp osm_eu.yml osm_eu.mml
+carto osm_eu.mml > osm_eu.xml
+rm osm_eu.mml
+
+echo ""
+echo ""
+
+ls -laGh osm_*.xml
+git status
+
+#sudo service renderd stop
+#sudo service renderd start
+
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  FIN"
+echo ""
