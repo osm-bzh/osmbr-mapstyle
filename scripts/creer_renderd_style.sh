d59cb6ecfd6e40005e8be8c4cc1eaf9ed36ed936
Maël <mael@breizhpodu.local>
2020-05-10 14:44:17 +0200

    nettoyage
    scripts obsolètes

--- scripts/creer_renderd_style.sh
+++
@@ -1,9 +0,0 @@
-#bin/sh!
-
-cd /data/osm/styles/osmbr/
-/data/osm/carto/bin/carto ./project.mml > style.xml
-
-#cp project.mml project_local.mml
-#sed -i 's/192.168.0.42/localhost/' project_local.mml
-#/data/osm/carto/bin/carto ./project_local.mml > style.xml
-
