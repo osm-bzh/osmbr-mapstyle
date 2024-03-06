4a0b366152a258fd50513563f7c82a82ff49299f
MaelREBOUX <m.reboux@rennesmetropole.fr>
2024-03-06 21:10:31 +0100

    monit : purge des tuiles

---
+++ ansible/roles/renderd_br/files/purge_tiles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+df -kh
+
+rm -rf /var/cache/renderd/tiles/br/20/
+rm -rf /var/cache/renderd/tiles/br/19/
+rm -rf /var/cache/renderd/tiles/br/18/
+rm -rf /var/cache/renderd/tiles/br/17/
+#rm -rf /var/cache/renderd/tiles/br/16/
+#rm -rf /var/cache/renderd/tiles/br/15/
+
+rm -rf /var/cache/renderd/tiles/eu/20/
+rm -rf /var/cache/renderd/tiles/eu/19/
+rm -rf /var/cache/renderd/tiles/eu/18/
+rm -rf /var/cache/renderd/tiles/eu/17/
+#rm -rf /var/cache/renderd/tiles/eu/16/
+#rm -rf /var/cache/renderd/tiles/eu/15/
+
+rm -rf /var/cache/renderd/tiles/oc/20/
+rm -rf /var/cache/renderd/tiles/oc/19/
+rm -rf /var/cache/renderd/tiles/oc/18/
+rm -rf /var/cache/renderd/tiles/oc/17/
+#rm -rf /var/cache/renderd/tiles/oc/16/
+#rm -rf /var/cache/renderd/tiles/oc/15/
+
+rm -rf /var/cache/renderd/tiles/gsw/20/
+rm -rf /var/cache/renderd/tiles/gsw/19/
+rm -rf /var/cache/renderd/tiles/gsw/18/
+rm -rf /var/cache/renderd/tiles/gsw/17/
+#rm -rf /var/cache/renderd/tiles/gsw/16/
+#rm -rf /var/cache/renderd/tiles/gsw/15/
+
+df -kh
+
